ca320fd8ee9b7c252f807c38c1e47471b300ae1a
Halil Ugur Ozmen <halil.ozmen@barikat.com.tr>
2024-04-24 00:02:13 +0300

    Day 1 Project of Angela Yu's 100 Days of Python

---
+++ day-1/main.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator.')
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's Your Pet's Name?\n")
+print("Your Band Name Could Be " + city + ' ' + pet_name)
